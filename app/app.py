2b58e121511fdec381e5364cfd695ccff322f101
henryvps <henryvps@gmail.com>
2024-08-06 08:57:44 +0700

    Initial PoC for asking Google LLMs to generate a Tweet for specific company based on their last 50 Tweets

--- app/app.py
+++ app/app.py
@@ -2,28 +2,50 @@ import pathlib
 import textwrap
 import os
 import google.generativeai as genai
+import json
 
 GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/ask/<question>', methods=['GET', 'POST'])
-def hello():
 
-	return ask_gemini(question))
+def json_to_string_with_keys(json_data):
+    """Converts JSON data into a string with keys and values."""
+    if not isinstance(json_data, dict):
+        raise ValueError("Input must be a JSON dictionary.")
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+    items = [f"{key}: {value}" for key, value in json_data.items()]
+    return ", ".join(items)
 
 
 
 def ask_gemini(req):
+    """Takes a String argument to Gemini for prompt tuning."""
     model = genai.GenerativeModel('gemini-1.5-flash')
     response = model.generate_content(req)
     return response.candidates[0].content.parts[0].text
-    
+
+def add_company_tweets_to_json(company, tweets):
+	"""Provides a company name and relevant Tweets we are working with to the JSON prompt"""
+	with open('./RISE.json', "r") as json_file:
+		data = json.load(json_file)
+		if 'COMPANY' in data:
+			data['COMPANY'] = company
+		if 'RISE.INPUT' in data:
+			data['RISE.INPUT'] = tweets
+	return data
 
 
+	
 
+
+@app.route('/ask', methods=['POST'])
+def ask():
+	
+	data = add_company_tweets_to_json(request.get_json()['COMPANY'], request.get_json()['INPUT'])
+	return ask_gemini(json_to_string_with_keys(data))
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8000)
