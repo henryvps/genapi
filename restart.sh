13b75b3854eb23e1410d72fa7b8bc1f526fc0c07
henryvps <henryvps@gmail.com>
2024-08-12 07:53:17 +0700

    Improved restart.sh and made extra sure RISE will not produce a Tweet without a handle.

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 docker rm -f `docker ps -aq`
-docker container prune
-docker image prune
+docker container prune -f
+docker image prune -f
 docker compose build --no-cache
 docker compose up -d
